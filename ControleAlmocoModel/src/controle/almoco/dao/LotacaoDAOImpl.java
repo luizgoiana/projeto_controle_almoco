6d1debd664144364ea2d3ea764d561ea2741aca6
luizgoiana <luizguilherme.goiana@gmail.com>
2014-09-16 17:46:52 -0300

    daoimpl

---
+++ ControleAlmocoModel/src/controle/almoco/dao/LotacaoDAOImpl.java
@@ -0,0 +1,40 @@
+package controle.almoco.dao;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+
+import controle.almoco.model.Lotacao;
+
+@Stateless
+public class LotacaoDAOImpl extends GenericDAOImpl<Lotacao> implements LotacaoDAO {
+
+	private final static String UNIT_NAME = "ControleAlmocoModel";
+	
+	@PersistenceContext(unitName = UNIT_NAME)
+    private EntityManager em;
+    
+    @Resource
+    UserTransaction ut;
+	
+	public LotacaoDAOImpl() {
+		super(Lotacao.class);
+	}
+	
+	
+	public void save(Lotacao lotacao) throws Exception {
+        super.save(lotacao);
+	}
+	
+	public List<Lotacao> findAll(){
+		return super.findAll();
+	}
+
+	
+	
+}
+
